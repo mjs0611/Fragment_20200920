dc9d5b229ca4b2e93d439a154389e8ed0be93c4f
JunSeung Mo <mojuns@gmail.com>
2020-09-20 16:21:09 +0900

    Fragment xml 작성

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20200920/Fragments/FirstFragment.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.fragment_20200920.Fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.tjoeun.fragment_20200920.R
+
+class FirstFragment : Fragment{
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+}
